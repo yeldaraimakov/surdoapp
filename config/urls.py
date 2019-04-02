4fbf1d5080b7d3357987472efef1f5ae177efbe2
yeldaraimakov <yeldar.aimakov@gmail.com>
2019-04-02 20:05:35 +0600

    url redirect

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='words_list', permanent=False)),
+
     path('django-admin/', admin.site.urls),
 
     path('admin/', include('surdoapp.admin_side.urls')),
